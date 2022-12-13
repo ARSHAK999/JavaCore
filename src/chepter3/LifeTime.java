e24b989736e8f2d1a999281db05e6f5cc2f927f7
7 <arshak.poleyan1999@gmail.com>
2022-12-14 00:59:12 +0400

    chepter3

---
+++ src/chepter3/LifeTime.java
@@ -0,0 +1,15 @@
+package chepter3;
+
+public class LifeTime {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 3; x++) {
+            int y = -1; // переменная у инициализируется при
+            // каждом вхождении в блок кода
+            System.out.println(" у равно : " + y);
+            // здесь всегда выводится значение -1
+            y = 100;
+            System.out.println(" y теперь равно " + y);
+        }
+    }
+}
