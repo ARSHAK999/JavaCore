28ac80cf6bb72905404478b20d6cdd1d9f8c7c17
7 <arshak.poleyan1999@gmail.com>
2022-12-20 00:48:36 +0400

    FigurePainter

---
+++ src/group1/claswork/IfForExample.java
@@ -0,0 +1,36 @@
+package group1.claswork;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 40;
+
+        if (a > b) {
+            System.out.println("max = " + a);
+        } else {
+            System.out.println("max = " + b);
+        }
+
+        System.out.println();
+
+        int c = 30;
+        int d = 35;
+
+        for (int i = c; i < d + 1; i++) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+
+        int h = 88;
+        char e = (char) h;
+
+        System.out.println(e);
+
+        if (a == b) {
+            System.out.println("a = b");
+        } else {
+            System.out.println("a =! b");
+        }
+    }
+}
+
