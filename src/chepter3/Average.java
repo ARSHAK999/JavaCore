f51c6fa21fbcf7787b1875b357e2273aef0fe33a
7 <arshak.poleyan1999@gmail.com>
2022-12-19 23:27:32 +0400

    Average

--- src/chepter3/Average.java
+++ src/chepter3/Average.java
@@ -2,15 +2,15 @@ package chepter3;
 
 public class Average {
     public static void main(String[] args) {
-        {
-            double nums[] = {10.1, 11.2, 12.3, 13.4, 14.5,};
-            double result = 0;
-            int i;
-            for (i = 0; i < 5; i++) ;
+        double numbers[] = {10.1, 11.2, 12.3, 13.4, 14.5};
+        double result = 0;
 
-            result = result + nums[i];
-            System.out.println("Среднее значение равно " + result / 5);
+        for (int i = 0; i < 5; i++) {
+            result = result + numbers[i];
 
         }
+        System.out.println("Numbers average = " + result / 5);
+
+
     }
 }
