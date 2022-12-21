d83a404d61f69c0c4f0d3a3017c655a6ebc78d3a
7 <arshak.poleyan1999@gmail.com>
2022-12-22 01:11:05 +0400

    FigurPainter2

---
+++ src/group1/claswork/FigurePainte2.java
@@ -0,0 +1,41 @@
+package group1.claswork;
+
+public class FigurePainte2 {
+    public static void main(String[] args) {
+        System.out.println("Figure 1");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 4; j > i; j--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        System.out.println("Figure 2");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 4; k > i; k--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
