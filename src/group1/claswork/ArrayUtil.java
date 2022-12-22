28ec0d2e9fc1fea507eeec5a2643a6417e8cc654
7 <arshak.poleyan1999@gmail.com>
2022-12-23 00:15:44 +0400

    ArrayUtil

---
+++ src/group1/claswork/ArrayUtil.java
@@ -0,0 +1,35 @@
+package group1.claswork;
+
+public class ArrayUtil {
+    public static void main(String[] args) {
+        int[] array = {5, 8, 14, 29, 25, 39, 26, 42, 45, 50};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        int x = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > x) ;
+            {
+                x = array[i];
+            }
+        }
+        System.out.println("max  " + x);
+
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]> x);{
+            }
+            x = array[i];
+            {
+            }
+            }
+            System.out.println("min " + x);
+        }
+    }
+
+
+
+
