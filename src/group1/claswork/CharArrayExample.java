228f7b57084889029de7962f6c4d40d0ce0b3ede
7 <arshak.poleyan1999@gmail.com>
2023-01-06 23:43:14 +0400

    CharArrayExample

---
+++ src/group1/claswork/CharArrayExample.java
@@ -0,0 +1,31 @@
+package group1.claswork;
+
+public class CharArrayExample {
+    public static void main(String[] args) {
+        char[] chars = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e', 'o'};
+        int count = 0;
+        char c = 'o';
+        for (int x : chars) {
+            if (x == c) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+
+        char[] chars2 = {'j', 'a', 'v', 'a', 'l', 'o', 'v', 'e'};
+        System.out.println(chars2[(chars2.length / 2) -1] + " " + chars2[(chars2.length / 2) ]);
+
+
+        char[] chars3 = {'j', 'a', 'v', 'a', 'l', 'o', 'l', 'y'};
+        System.out.println(chars3[chars3.length - 2] == 'l' && chars3[chars3.length - 1] == 'y');
+
+
+        char[] text = {' ', ' ', 'b', 'a', 'r', 'e', 'v', ' ', ' '};
+        for (char x : text) {
+            if (x != ' ') {
+                System.out.print(x + " ");
+            }
+        }
+    }
+}
