2b7d9f2e5254603cd5fec1d1ca9cdb1846c9a6c8
7 <arshak.poleyan1999@gmail.com>
2023-01-09 01:26:16 +0400

    homework

---
+++ src/homwork5/ArraySpaceExample.java
@@ -0,0 +1,29 @@
+package homwork5;
+
+public class ArraySpaceExample {
+    public static void main(String[] args) {
+        char[] spaceArray = {' ', ' ', 'd', 'o', 'g', ' ', 'c', 'a', 't', 's', ' ', ' ', ' '};
+
+        int indexOne = 0;
+        int indexTwo = spaceArray.length - 1;
+
+
+        while (indexOne < spaceArray.length && spaceArray[indexOne] == ' ') {
+            indexOne++;
+        }
+        while (indexTwo >= 0 && spaceArray[indexTwo] == ' ') {
+            indexTwo--;
+        }
+
+        char[] resultChars = new char[indexTwo - indexOne + 1];
+        for (int i = 0; i < resultChars.length; i++) {
+            resultChars[i] = spaceArray[indexOne];
+            indexOne++;
+            System.out.print(resultChars[i]);
+
+        }
+
+    }
+}
+
+
