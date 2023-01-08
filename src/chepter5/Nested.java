42cc3be16a39ad9ad4fb7f7d20fc43283ad6e13d
7 <arshak.poleyan1999@gmail.com>
2023-01-09 01:39:27 +0400

    chepter5

--- src/chepter5/Nested.java
+++ src/chepter5/Nested.java
@@ -1,14 +1,14 @@
-package  claswork.homework1;
+package chepter5;
 
 public class Nested {
     public static void main(String[] args) {
         int i, j;
         for (i = 0; i < 10; i++) {
 
-            for (j = i; j < 10; j++) {
+            for (j = i; j <= 10; j++) {
 
                 System.out.print(".");
-                System.out.println( );
+                System.out.println();
             }
         }
     }
