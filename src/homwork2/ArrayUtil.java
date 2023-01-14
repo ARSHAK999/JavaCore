490047b60bf073d72c3e52e915affbb849611f7a
7 <arshak.poleyan1999@gmail.com>
2023-01-14 22:39:40 +0400

    ArrayUtil

--- src/homwork2/ArrayUtil.java
+++ src/homwork2/ArrayUtil.java
@@ -2,31 +2,36 @@ package homwork2;
 
 public class ArrayUtil {
     public static void main(String[] args) {
-        int[] array = {5, 8, 14, 29, 25, 39, 26, 42, 45, 80};
+        int[] array = {2, 5, 8, 10, 32, 22,  66, 12, 30, 0, 44,-9};
+
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
 
-        int x = 0;
+        int a = array[0];
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] > x) ;
-            {
-                x = array[i];
+            if (array[i] > a) {
+                a = array[i];
+
             }
         }
-        System.out.println("max  " + x);
+        System.out.println("max =  " + a);
 
-
+        int u  = 18;
         for (int i = 0; i < array.length; i++) {
-            if (x > array[i]);{
-                x = array[0];
+            if (u > array[i]) {
+                u = array[i];
+
             }
-            }
-            System.out.println("min  " + x);
+            System.out.println("min = " + u);
         }
+
     }
+}
+
+
 
 
 
