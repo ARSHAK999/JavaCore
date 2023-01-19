95c6c1d66b34482425ec69b2022b709c6d9adf82
7 <arshak.poleyan1999@gmail.com>
2023-01-20 01:27:28 +0400

    chepter7

---
+++ src/chepter7/OverLoadCons.java
@@ -0,0 +1,17 @@
+package chepter7;
+
+public class OverLoadCons {
+    public static void main(String[] args) {
+        Box mybox1 = new Box(10, 20, 15);
+        Box mybox2 = new Box();
+        Box mycube = new Box(7);
+        double vol;
+        vol = mybox1.volume();
+        System.out.println("obiem mybox1 >" + vol);
+        vol = mybox2.volume();
+        System.out.println("obiem mybox2 >" + vol);
+        vol = mycube.volume();
+        System.out.println("obiem mycube > " + vol);
+    }
+
+}
