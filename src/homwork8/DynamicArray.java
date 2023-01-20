c41105b96bbfe16133985a4b147988b114722a2e
7 <arshak.poleyan1999@gmail.com>
2023-01-20 23:58:15 +0400

    homework8

--- src/homwork8/DynamicArray.java
+++ src/homwork8/DynamicArray.java
@@ -14,6 +14,12 @@ public class DynamicArray {
     }
 
     private void extend() {
+        int array2[] = new int[array.length + 10];
+        for (int i = 0; i < size; i++) {
+            array2[i] = array[i];
+
+        }
+        array = array2;
     }
 
     public int get(int index){
