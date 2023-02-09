fa9aeae029de49ee98e4a668f85437e0e8a7e0a1
7 <arshak.poleyan1999@gmail.com>
2023-02-10 02:41:32 +0400

    homework10

---
+++ src/homwork10/Employee.java
@@ -0,0 +1,98 @@
+package homwork10;
+
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private String id;
+    private double salary;
+    private String company;
+    private String position;
+
+
+    public Employee(String name, String surname, String id, double salary, String company, String position) {
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+        this.salary = salary;
+        this.company = company;
+        this.position = position;
+    }
+
+    public Employee() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Double.compare(employee.salary, salary) == 0 && Objects.equals(name, employee.name) && Objects.equals(surname, employee.surname) && Objects.equals(id, employee.id) && Objects.equals(company, employee.company) && Objects.equals(position, employee.position);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, id, salary, company, position);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", id='" + id + '\'' +
+                ", salary=" + salary +
+                ", company='" + company + '\'' +
+                ", position='" + position + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+}
---
+++ src/homwork10/EmployeeStorage.java
@@ -0,0 +1,67 @@
+package homwork10;
+
+public class EmployeeStorage {
+    private Employee[] array = new Employee[10];
+    private int size;
+
+
+    public void add(Employee item) {
+        if (size == array.length) {
+            extend();
+        }
+        array[size++] = item;
+    }
+
+
+    public void print() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i] + " ");
+        }
+    }
+
+
+    public void extend() {
+        Employee[] array2 = new Employee[array.length + 10];
+        for (int i = 0; i < array.length; i++) {
+            array2[i] = array[i];
+        }
+        array = array2;
+    }
+
+
+    public Employee search(String ID) {
+        for (int i = 0; i < size; i++) {
+            Employee employee = array[i];
+            if (employee.getID().toLowerCase().contains(ID.toLowerCase())) {
+                return (employee);
+            }
+        }
+        return null;
+    }
+
+    public String checkID(String id) {
+        for (int i = 0; i < size; i++) {
+            Employee employee = array[i];
+            String tmp = employee.getID();
+            if (id.equals(tmp)) {
+                return null;
+            }
+        }
+        return id;
+    }
+
+    public void searchByCompanyName(String companyNameSearch) {
+        boolean found = false;
+        for (int i = 0; i < size; i++) {
+            Employee employee = array[i];
+            if (employee.getCompany().toLowerCase().contains(companyNameSearch.toLowerCase())) {
+                found = true;
+                System.out.println(employee.getName() + " " + employee.getSurname());
+            }
+        }
+        if (!found) {
+            System.out.println("company " + companyNameSearch + "does not exits");
+        }
+
+    }
+}
