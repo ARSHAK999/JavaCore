028d47ae6224a3b006abc73ecf8d9b7938c0a79a
7 <arshak.poleyan1999@gmail.com>
2023-02-09 23:41:17 +0400

    homework8

--- src/homwork8/DynamicArray.java
+++ src/homwork8/DynamicArray.java
@@ -4,10 +4,10 @@ import static jdk.nashorn.internal.objects.NativeJava.extend;
 
 public class DynamicArray {
     private int[] array = new int[10];
-    private  int size = 0 ;
+    private int size = 0;
 
-    public void add(int item){
-        if(size == array.length){
+    public void add(int item) {
+        if (size == array.length) {
             extend();
         }
         array[size++] = item;
@@ -22,17 +22,69 @@ public class DynamicArray {
         array = array2;
     }
 
-    public int get(int index){
+    public int get(int index) {
         if (index >= array.length) {
             return -1;
         }
         return array[index];
-        }
-        public  void print(){
-            for (int i : array) {
-                System.out.print(i + " ");
+    }
 
+    public void print() {
+        for (int i : array) {
+            System.out.print(i + " ");
+
+        }
+    }
+
+    public void deleteByIndex(int index) {
+        if (index < 0 || index > size - 1) {
+            System.out.println("Index");
+            return;
+        }
+        for (int i = index + 1; i < size; i++) {
+            array[i - 1] = array[i];
+
+        }
+        size--;
+    }
+
+    public void set(int index, int value) {
+        if (index < 0 || index > size - 1) {
+            System.out.println("Index");
+            return;
+        }
+        array[index] = value;
+    }
+    public void add(int index,int value){
+        if (index < 0 || index > size -1){
+            System.out.println("index");
+            return;
+        }
+        if (size == array.length){
+            extend();
+        }
+        for (int i = size; i >index; i--){
+            array[i] = array[i-1];
+        }
+        array[index] = value;
+        size++;
+    }
+    public  boolean exists(int value) {
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value) {
+                return true;
             }
         }
-}
 
+        return false;
+    }
+    public int getIndexByValue(int value){
+        for (int i = 0; i < size; i++) {
+            if (array[i] == value){
+                return i;
+            }
+
+        }
+        return  -1;
+    }
+    }
--- src/homwork8/DynamicArrayDemo.java
+++ src/homwork8/DynamicArrayDemo.java
@@ -2,12 +2,31 @@ package homwork8;
 
 public class DynamicArrayDemo {
     public static void main(String[] args) {
-        DynamicArray  nums = new DynamicArray();
-        for (int i = 0; i < 20; i++) {
-            nums.add(i);
+        DynamicArray  dy = new DynamicArray();
+        for (int i = 0; i < 10; i++) {
+            dy.add(i + 1);
         }
-        nums.print();
+        dy.print();
         System.out.println();
-        System.out.println(nums.get(0));
+        System.out.println(dy.get(0));
+
+
+        dy.deleteByIndex(4);
+        dy.print();
+
+        System.out.println();
+        dy.set(3,88);
+        dy.print();
+        System.out.println();
+
+        dy.print();
+        dy.add(5,99);
+
+
+        System.out.println(dy.exists(8));
+        System.out.println(dy.getIndexByValue(7));
+
+
     }
+
 }
