5c1cffafd23c30fca5d8b761bc5a479ad0264759
7 <arshak.poleyan1999@gmail.com>
2023-03-21 00:52:38 +0400

    chepter11

---
+++ src/chepter11/CallMe/CallMe.java
@@ -0,0 +1,13 @@
+package chepter11.CallMe;
+
+public class CallMe {
+    void call(String msg) {
+        System.out.print("[" + msg);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("]");
+    }
+}
